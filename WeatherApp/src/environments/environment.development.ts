31a304addf6cfeb66fb45e8a7d8193331a7ddcc8
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-12-14 21:23:56 -0300

    fixed to metric system

--- WeatherApp/src/environments/environment.development.ts
+++ WeatherApp/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    weatherApiBaseUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/rio%20grande',
+    weatherApiBaseUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/rio%20grande?unitGroup=metric&',
     weatherApiKey: '7ETM7RX43BNSGZZCSDHZW5XTU',
 };
